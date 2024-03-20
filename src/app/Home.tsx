97f97d5f7b137598c76251beb37192f95dfad654
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2024-03-20 20:18:28 -0300

    checkpoint: base mobile project

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -2,7 +2,7 @@ import { View, Text } from "react-native";
 
 export function Home() {
   return (
-    <View className="flex flex-1 justify-center items-center bg-red-500">
+    <View className="flex flex-1 justify-center items-center bg-green-500">
       <Text>Hello world</Text>
     </View>
   );
