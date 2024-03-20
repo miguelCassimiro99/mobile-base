13c94ed4b75e6d1c4c78fa54bfa4a0b771bf45df
Miguel Cassimiro <miguel.cassimiro99@gmail.com>
2024-03-20 20:26:26 -0300

    chore: changed style

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import "./src/styles/global";
 
 import { Home } from "./src/app/Home";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
     <>
+      <StatusBar style="light" />
       <Home />
     </>
   );
--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -2,7 +2,7 @@ import { View, Text } from "react-native";
 
 export function Home() {
   return (
-    <View className="flex flex-1 justify-center items-center bg-green-500">
+    <View className="flex flex-1 justify-center items-center bg-gray-700">
       <Text>Hello world</Text>
     </View>
   );
